93b19e4d8799675cd7642ae66836276996adbce3
Slaszor <35063479+Slaszor@users.noreply.github.com>
2023-01-23 21:57:14 +0100

    Sets aws_s3_bucket_public_access_block to block public access

--- common-infrastructure/storage/s3-tfstate.tf
+++ common-infrastructure/storage/s3-tfstate.tf
@@ -22,3 +22,9 @@ resource "aws_s3_bucket_versioning" "tfstate" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  block_public_acls   = true
+  block_public_policy = true
+ }
